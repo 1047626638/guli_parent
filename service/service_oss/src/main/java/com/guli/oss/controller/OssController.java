1652493c084f981eac78c7a5436294e9a7d68ed2
李鹏 <jr_lipeng@yonyou.com>
2022-11-04 16:33:01 +0800

    easyExcel demo

--- service/service_oss/src/main/java/com/guli/oss/controller/OssController.java
+++ service/service_oss/src/main/java/com/guli/oss/controller/OssController.java
@@ -5,6 +5,7 @@ import com.guli.oss.service.OssService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequestMapping("/eduoss/fileoss")
 @Api("文件上传到阿里云OSS")
+@CrossOrigin //解决跨域
 public class OssController {
     @Autowired
     private OssService ossService;
